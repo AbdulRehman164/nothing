ac1ab4dea9bf50d4797beee8c559bf93e8476092
Abdul-Rehman164 <63417636+Abdul-Rehman164@users.noreply.github.com>
2023-03-30 23:52:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-const button = document.createElement("button"); 
-  document.body.appendChild(button);   button.onclick = function () {         location.href = "https://www.youtube.com";     };
+// const button = document.createElement("button"); 
+//   document.body.appendChild(button);   button.onclick = function () {         location.href = "https://www.youtube.com";     };
+alert("hello");
