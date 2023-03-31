9ccbd3b14f45c7ecaf5d0c176bce19703878f7c0
Abdul-Rehman164 <63417636+Abdul-Rehman164@users.noreply.github.com>
2023-03-31 22:28:01 +0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,4 +3,4 @@ function displayImage(imageUrl) {
   img.src = imageUrl;  // set the src attribute to the image URL
   document.body.appendChild(img);  // append the <img> element to the body of the HTML document
 }
-displayImage('https://images.app.goo.gl/EQTTYziqzDrj1AYo6');
+displayImage('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80');
