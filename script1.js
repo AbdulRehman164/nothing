b5b463547c51d303b9f572f1d83bb364b10ffee8
Abdul-Rehman164 <63417636+Abdul-Rehman164@users.noreply.github.com>
2023-03-31 23:04:24 +0500

    Update and rename script2.js to script1.js

---
+++ script1.js
@@ -0,0 +1,17 @@
+<function displayImageInModal(imageUrl) {
+  const modalContainer = document.createElement('div');
+  modalContainer.classList.add('modal-container');
+  const modalOverlay = document.createElement('div');
+  modalOverlay.classList.add('modal-overlay');
+  modalOverlay.addEventListener('click', closeModal);
+  const modalImage = document.createElement('img');
+  modalImage.src = imageUrl;
+  modalImage.classList.add('modal-image');
+  modalContainer.appendChild(modalOverlay);
+  modalContainer.appendChild(modalImage);
+  document.body.appendChild(modalContainer);
+  function closeModal() {
+    modalContainer.remove();
+  }
+}
+displayImageInModal('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80');
--- script2.js
+++
@@ -1,6 +0,0 @@
-function displayImage(imageUrl) {
-  const img = document.createElement('img');  // create a new <img> element
-  img.src = imageUrl;  // set the src attribute to the image URL
-  document.body.appendChild(img);  // append the <img> element to the body of the HTML document
-}
-displayImage('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80');
