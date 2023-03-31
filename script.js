b150864a91b0a1a35ceb9031ec2c27b82703da80
Abdul-Rehman164 <63417636+Abdul-Rehman164@users.noreply.github.com>
2023-03-31 22:09:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("hello");
+function displayImage(imageUrl) {
+  const img = document.createElement('img');  // create a new <img> element
+  img.src = imageUrl;  // set the src attribute to the image URL
+  document.body.appendChild(img);  // append the <img> element to the body of the HTML document
+}
+displayImage('https://images.app.goo.gl/D2iPYspD4QpfdqWV9');
