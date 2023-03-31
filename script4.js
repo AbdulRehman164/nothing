90c9541a084316f5f63a5188ac67538f61e841ca
Abdul-Rehman164 <63417636+Abdul-Rehman164@users.noreply.github.com>
2023-03-31 23:17:22 +0500

    Update script4.js

--- script4.js
+++ script4.js
@@ -19,4 +19,4 @@ function displayImageInModal(imageUrl) {
   }
 }
 
-displayImageInModal('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80');
+//displayImageInModal('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80');
